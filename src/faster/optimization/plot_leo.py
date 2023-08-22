b6a5f1f0a61ca419301460da448957216cc89189
LeoAlf <leonardo.alfonso@gmail.com>
2023-08-22 11:22:55 +0200

    Added plot_leo.py, with codes to analyse the results produced by the no_pumping.py code. The analysis include the generation of the pareto front and the generation of maps for any solution.
    It now reads solutions for experiments no_pumping and genetic_pumping - codes are rearranged to separate no_pump and genetic_pump solutionsCode is better commented.

--- src/faster/optimization/plot_leo.py
+++ src/faster/optimization/plot_leo.py
@@ -7,38 +7,32 @@ import matplotlib.pyplot as plt
 # Generic file_graph_nodes locations
 csv_file_graph_information = 'D:\\OP_pycharm\\Operwas_pump\\inputs\\wwtp_locations_many.csv'
 
-# Files no pumping
-# 'D:\OP_pycharm\Operwas_pump\optimization_results\no_pumping\run_2023-08-18-17-18-52-125\solutions_2023-08-18-17-18-52-125.csv'
-# 'D:\\OP_pycharm\\Operwas_pump\\optimization_results\\no_pumping\\run_2023-08-18-17-18-52-125\\solutions_2023-08-18-17-18-52-125'
-# experiment_folder = 'D:\\OP_pycharm\\Operwas_pump\\optimization_results\\no_pumping\\'
-# experiment_id = 'run_2023-08-18-17-18-52-125'
-# experiment_full_path = experiment_folder + experiment_id + '\\'
-
-# Files genetic pumping
-# 'D:\OP_pycharm\Operwas_pump\optimization_results\genetic_pumping\run_2023-08-20-16-38-44-738\solutions_2023-08-20-16-38-44-738.csv'
+# ************************ Visualising results genetic pumping ****************************
+# ******in 'D:\OP_pycharm\Operwas_pump\optimization_results\genetic_pumping\ **************
+# *****************************************************************************************
 
 experiment_folder = 'D:\\OP_pycharm\\Operwas_pump\\optimization_results\\genetic_pumping\\'
 experiment_id = 'run_2023-08-20-16-38-44-738'
 experiment_full_path = experiment_folder + experiment_id + '\\'
 
 # CSV Files
-csv_file_wwtp_labels = experiment_full_path + 'solutions' + experiment_id.lstrip("run") + '.csv'  # solutions
-csv_file_wwtp_results_total = experiment_full_path + 'results_total' + experiment_id.lstrip(
+csv_file_gen_pump_solutions = experiment_full_path + 'solutions' + experiment_id.lstrip("run") + '.csv'  # solutions
+csv_file_gen_pump_results_total = experiment_full_path + 'results_total' + experiment_id.lstrip(
     "run") + '.csv'  # results_total
 
 # Read CSV file with graph information
 graph_data = pd.read_csv(csv_file_graph_information)
 
-# Read CSV file with 'WWTP' labels
-wwtp_labels = pd.read_csv(csv_file_wwtp_labels)
+# Read CSV file with 'WWTP' labels genetic_pumping
+wwtp_labels = pd.read_csv(csv_file_gen_pump_solutions)
 
-# Read CSV file with information about the WWTP configuration
-overall_info = pd.read_csv(csv_file_wwtp_results_total)
+# Read CSV file with information about the WWTP configuration with no pumping
+overall_info = pd.read_csv(csv_file_gen_pump_results_total)
 
 
 # Create directed graph the results of genetic_pumping
 
-def generate_and_plot_graph(row_index, graph_nodes_data, graph_connections_data, df_overall_info, save_path=None):
+def generate_graph_with_pumping(row_index, graph_nodes_data, graph_connections_data, df_overall_info, save_path=None):
     # Create a directed graph (DiGraph)
     G = nx.DiGraph()
 
@@ -104,8 +98,7 @@ def generate_and_plot_graph(row_index, graph_nodes_data, graph_connections_data,
 
     plt.gca().text(1.05, 0.5, text_box, transform=plt.gca().transAxes, fontsize=10, verticalalignment='center')
 
-
-    plt.title('Graph solution ' + str(row_index) + ' in file results ' + experiment_id)
+    plt.title('Solution with pumps, ' + str(row_index) + ' in file results ' + experiment_id)
     plt.xlabel('X-coordinate')
     plt.ylabel('Y-coordinate')
     plt.grid(True)
@@ -118,118 +111,37 @@ def generate_and_plot_graph(row_index, graph_nodes_data, graph_connections_data,
 
 # Call the function with the desired row index
 run_id = 4900
-generate_and_plot_graph(run_id, graph_data, wwtp_labels, overall_info, save_path='experiment_full_path' + 'graph.png')  # Replace with the desired row index
-
-# Optimisation results to analyse
-
-# ************************** Hossein***********************
-
-# Read edges CSV file
-edges_data = pd.read_csv(
-    r'D:\IHE-Delft\2023\MSc 3-month theses\Hossein Tavakoligargari 2023\model\pipes.csv')  # Replace with your file path
-# Read nodes CSV file
-nodes_data = pd.read_csv(
-    r'D:\IHE-Delft\2023\MSc 3-month theses\Hossein Tavakoligargari 2023\model\manholes.csv')  # Replace with your file path
-
-# Create a graph
-G = nx.DiGraph()
-
-# Add nodes with attributes
-for _, node_row in nodes_data.iterrows():
-    node_name = node_row['Name']
-    x_coord = node_row['X-Coord']
-    y_coord = node_row['Y-Coord']
-    G.add_node(node_name, x=x_coord, y=y_coord)
-    # You can also add other attributes as needed
-
-# Add edges with attributes
-for _, edge_row in edges_data.iterrows():
-    from_node = edge_row['From Node']
-    to_node = edge_row['To Node']
-    edge_name = edge_row['Name']
-    G.add_edge(from_node, to_node, name=edge_name)
-    # You can also add other attributes as needed
-
-# # Draw the graph
-# node_positions = {node_name: (x_coord, y_coord) for node_name, x_coord, y_coord in nodes_data[['Name', 'X-Coord', 'Y-Coord']].values}
-# nx.draw(G, pos=node_positions, with_labels=True, font_weight='bold', node_size=100)
-# #edge_labels = nx.get_edge_attributes(G, 'name')
-# #nx.draw_networkx_edge_labels(G, pos=node_positions, edge_labels=edge_labels
-#
-# plt.title('Graph with Nodes and Edges')
-# plt.xlabel('X-coordinate')
-# plt.ylabel('Y-coordinate')
-# plt.grid(False)
-# plt.show()
-
-given_node_id = 331
+generate_graph_with_pumping(run_id, graph_data, wwtp_labels, overall_info,
+                            save_path='experiment_full_path' + 'graph.png')  # Replace with the desired row index
 
 
-# Function to get upstream nodes recursively
-def get_upstream_nodes_recursive(graph, node, visited=None):
-    if visited is None:
-        visited = set()
-    visited.add(node)
-    for predecessor in graph.predecessors(node):
-        if predecessor not in visited:
-            get_upstream_nodes_recursive(graph, predecessor, visited)
-    return visited
+# ***************************** Visualising results no_pumping ****************************
+# ***********in 'D:\OP_pycharm\Operwas_pump\optimization_results\no_pumping\ **************
+# *****************************************************************************************
+
+# Files no pumping
+# 'D:\OP_pycharm\Operwas_pump\optimization_results\no_pumping\run_2023-08-18-17-18-52-125\solutions_2023-08-18-17-18-52-125.csv'
+# 'D:\\OP_pycharm\\Operwas_pump\\optimization_results\\no_pumping\\run_2023-08-18-17-18-52-125\\solutions_2023-08-18-17-18-52-125'
+experiment_folder = 'D:\\OP_pycharm\\Operwas_pump\\optimization_results\\no_pumping\\'
+experiment_id = 'run_2023-08-18-17-18-52-125'
+experiment_full_path = experiment_folder + experiment_id + '\\'
+
+# CSV Files
+csv_file_no_pump_solutions = experiment_full_path + 'solutions' + experiment_id.lstrip("run") + '.csv'  # solutions
+csv_file_no_pump_results_total = experiment_full_path + 'results_total' + experiment_id.lstrip(
+    "run") + '.csv'  # results_total
+
+# Read CSV file with graph information
+graph_data = pd.read_csv(csv_file_graph_information) #(already read in the beginning of this file)
+
+# Read CSV file with 'WWTP' labels no_pumping
+wwtp_labels = pd.read_csv(csv_file_no_pump_solutions)
+
+# Read CSV file with information about the WWTP configuration with no pumping
+overall_info = pd.read_csv(csv_file_no_pump_results_total)
 
 
-# Get upstream nodes (all predecessors)
-upstream_nodes = get_upstream_nodes_recursive(G, given_node_id)
-
-# Convert to DataFrame
-upstream_nodes_df = pd.DataFrame({'NodeID': list(upstream_nodes)})
-
-print(upstream_nodes_df)
-
-
-# ****************************************** HOSSEIN ******************
-
-
-# Produce graph with wttp locations, connections and type of ndoes
-
-
-# # Plotting last generation:
-# coverage_last_gen = overall_info.loc[overall_info['idx_evaluation'] >= 0, 'Coverage (region)']
-# ben_cos_last_gen = overall_info.loc[overall_info['idx_evaluation'] >= 0, 'Benefits/costs']
-# central_last_gen = overall_info.loc[overall_info['idx_evaluation'] >= 0, 'Centralization degree Huang']
-# num_wwtp_last_gen = overall_info.loc[overall_info['idx_evaluation'] >= 0, 'Number WWTP']
-#
-# # Identify Pareto front points
-# pareto_front = []
-# for i in range(len(coverage_last_gen)):
-#     is_pareto = True
-#     for j in range(len(coverage_last_gen)):
-#         if i != j and coverage_last_gen[j] >= coverage_last_gen[i] and ben_cos_last_gen[j] >= ben_cos_last_gen[i]:
-#             is_pareto = False
-#             break
-#     if is_pareto:
-#         pareto_front.append((coverage_last_gen[i], ben_cos_last_gen[i]))
-#
-# # Plotting all generations:
-# scatter_plot = overall_info.plot.scatter(x='Coverage (region)', y='Benefits/costs', c='Centralization degree Huang', s='Number WWTP')
-#
-# # Plot the Pareto front points on top
-# pf_x = [point[0] for point in pareto_front]
-# pf_y = [point[1] for point in pareto_front]
-# scatter_plot.scatter(pf_x, pf_y, label='Pareto Front', color='black', marker='o', alpha=.1, s=40)
-#
-# # Annotate Pareto front points with their indices
-# for i, point in enumerate(pareto_front):
-#     scatter_plot.annotate(f'{i}', (point[0], point[1]), textcoords="offset points", xytext=(0, 10), ha='center')
-#
-#
-# scatter_plot.legend()
-# #scatter_plot.title('Scatter Plot with Pareto Front')
-# scatter_plot.grid(True)
-# plt.show(block=True)
-
-
-# Function to generate a graph (when user clicks scatter with Pareto)
-def generate_graph(selected_row, df_graph_data, df_wwtp_labels, df_overall_info):
-    #    plt.figure()  # Create a new figure for the graph
+def generate_graph_no_pumping(selected_row, df_graph_data, df_wwtp_labels, df_overall_info):
     # ... Generate the graph using selected_row data
 
     # Get WWTP labels for nodes
@@ -300,33 +212,22 @@ def generate_graph(selected_row, df_graph_data, df_wwtp_labels, df_overall_info)
 
     plt.gca().text(1.05, 0.5, text_box, transform=plt.gca().transAxes, fontsize=10, verticalalignment='center')
 
-    plt.title('Configuration of WWTPs - solution ' + str(selected_row))
+    plt.title('Configuration of WWTPs - solution with no pumps' + str(selected_row))
     plt.xlabel('X-axis')
     plt.ylabel('Y-axis')
     plt.grid(True)
     plt.show()
 
 
-# Create a function to handle click event
-def on_click(event):
-    if event.button == 1:  # Left mouse button click
-        print('Clicked!')
-        ind = event.ind[0]  # Get the index of the clicked point
-        print('Index is: ' + str(ind))
-        selected_row = overall_info.iloc[ind]  # Get the corresponding row from the DataFrame
-        generate_graph(selected_row, graph_data, overall_info)  # Call the generate_graph function with selected_row
-
-
-# # Connect the pick event to the generate_graph function
-# scatter_plot.figure.canvas.mpl_connect('pick_event', generate_graph)
+generate_graph_no_pumping(63043, graph_data, wwtp_labels, overall_info)   #63043 is a good solution
 
 # **************************************
-# plot WWTP locations
+# plot WWTP locations - This
 
 #
 # # Create a graph
 # G = nx.Graph()
-#
+
 # # Add nodes with coordinates
 # for index, row in data.iterrows():
 #     G.add_node(row['id'], pos=(row['X'], row['Y']))
@@ -345,7 +246,108 @@ def on_click(event):
 # plt.figure(figsize=(6, 10))
 # plt.gca().set_aspect('equal', adjustable='box')  # Set equal aspect ratio
 # nx.draw(G, pos=node_positions, with_labels=True, font_weight='bold', node_size=500, node_color='lightblue')
-# plt.title('Graph from CSV')
+# plt.title('Connection between candidate points according to natural flow in catchments')
 # plt.show()
 
-generate_graph(63043, graph_data, wwtp_labels, overall_info)
+# Optimisation results to analyse
+# *****************************************************************************
+# Plotting given generations with identified Pareto front:
+# *****************************************************************************
+coverage_last_gen = overall_info.loc[overall_info['idx_evaluation'] >= 0, 'Coverage (region)']
+ben_cos_last_gen = overall_info.loc[overall_info['idx_evaluation'] >= 0, 'Benefits/costs']
+central_last_gen = overall_info.loc[overall_info['idx_evaluation'] >= 0, 'Centralization degree Huang']
+num_wwtp_last_gen = overall_info.loc[overall_info['idx_evaluation'] >= 0, 'Number WWTP']
+
+# Identify Pareto front points
+pareto_front = []
+for i in range(len(coverage_last_gen)):
+    is_pareto = True
+    for j in range(len(coverage_last_gen)):
+        if i != j and coverage_last_gen[j] >= coverage_last_gen[i] and ben_cos_last_gen[j] >= ben_cos_last_gen[i]:
+            is_pareto = False
+            break
+    if is_pareto:
+        pareto_front.append((coverage_last_gen[i], ben_cos_last_gen[i]))
+
+# Plotting all generations:
+scatter_plot = overall_info.plot.scatter(x='Coverage (region)', y='Benefits/costs', c='Centralization degree Huang', s='Number WWTP')
+
+# Plot the Pareto front points on top
+pf_x = [point[0] for point in pareto_front]
+pf_y = [point[1] for point in pareto_front]
+scatter_plot.scatter(pf_x, pf_y, label='Pareto Front', color='black', marker='o', alpha=.1, s=40)
+
+# Annotate Pareto front points with their indices
+for i, point in enumerate(pareto_front):
+    scatter_plot.annotate(f'{i}', (point[0], point[1]), textcoords="offset points", xytext=(0, 10), ha='center')
+
+
+scatter_plot.legend()
+scatter_plot.grid(True)
+plt.show(block=True)
+
+# ***************************************************************************************************************
+# ************************** Hossein - uses graphs to facilitate analysis of SWIMM models ***********************
+# ***************************************************************************************************************
+
+# Read edges CSV file
+edges_data = pd.read_csv(
+    r'D:\IHE-Delft\2023\MSc 3-month theses\Hossein Tavakoligargari 2023\model\pipes.csv')  # Replace with your file path
+# Read nodes CSV file
+nodes_data = pd.read_csv(
+    r'D:\IHE-Delft\2023\MSc 3-month theses\Hossein Tavakoligargari 2023\model\manholes.csv')  # Replace with your file path
+
+# Create a graph
+G = nx.DiGraph()
+
+# Add nodes with attributes
+for _, node_row in nodes_data.iterrows():
+    node_name = node_row['Name']
+    x_coord = node_row['X-Coord']
+    y_coord = node_row['Y-Coord']
+    G.add_node(node_name, x=x_coord, y=y_coord)
+    # You can also add other attributes as needed
+
+# Add edges with attributes
+for _, edge_row in edges_data.iterrows():
+    from_node = edge_row['From Node']
+    to_node = edge_row['To Node']
+    edge_name = edge_row['Name']
+    G.add_edge(from_node, to_node, name=edge_name)
+    # You can also add other attributes as needed
+
+# # Draw the graph (this takes time)
+# node_positions = {node_name: (x_coord, y_coord) for node_name, x_coord, y_coord in nodes_data[['Name', 'X-Coord', 'Y-Coord']].values}
+# nx.draw(G, pos=node_positions, with_labels=True, font_weight='bold', node_size=100)
+# #edge_labels = nx.get_edge_attributes(G, 'name')
+# #nx.draw_networkx_edge_labels(G, pos=node_positions, edge_labels=edge_labels
+#
+# plt.title('Graph with Nodes and Edges')
+# plt.xlabel('X-coordinate')
+# plt.ylabel('Y-coordinate')
+# plt.grid(False)
+# plt.show()
+
+given_node_id = 331
+
+
+# Function to get upstream nodes recursively
+def get_upstream_nodes_recursive(graph, node, visited=None):
+    if visited is None:
+        visited = set()
+    visited.add(node)
+    for predecessor in graph.predecessors(node):
+        if predecessor not in visited:
+            get_upstream_nodes_recursive(graph, predecessor, visited)
+    return visited
+
+
+# Get upstream nodes (all predecessors)
+upstream_nodes = get_upstream_nodes_recursive(G, given_node_id)
+
+# Convert to DataFrame
+upstream_nodes_df = pd.DataFrame({'NodeID': list(upstream_nodes)})
+
+print(upstream_nodes_df)
+
+# ****************************************** HOSSEIN ******************
